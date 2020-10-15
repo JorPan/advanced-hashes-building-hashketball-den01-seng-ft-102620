a6ea0f5bf7fb7a465ca83f67e0c0850b2568f3a4
Jordan Panasewicz <jbpanasewicz@gmail.com>
2020-10-15 15:18:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,10 +3,23 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => {
-        
-      }
+      :players => [{
+        :player_name => "Alan Anderson"
+        :number
+        :shoe
+        :points
+        :rebounds
+        :assists
+        :steals
+        :blocks
+        :slam_dunks
+      },
       
+      
+      
+      
+      
+      ]
     },
     :away => {
       :team_name => "Charlotte Hornets",
